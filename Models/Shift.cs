b015e7bcab51a25effa67d0979d7ea41478e7e5f
Joshua Tingler <jtingler96@gmail.com>
2022-01-26 16:09:26 +0300

    initial

---
+++ Models/Shift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShiftApi
+{
+    public class Shift
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public decimal HourlyRate { get; set; }
+        public DateTime? ClockIn { get; set; }
+        public DateTime? ClockOut { get; set; }
+        public TimeSpan? HoursWorked { get; set; }
+        public decimal ShiftEarnings { get; set; }
+    }
+}
